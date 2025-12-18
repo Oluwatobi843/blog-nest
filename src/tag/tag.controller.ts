628f586ed9e5f6dc9a71add82ce8ddf412beaaa0
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-12-18 11:09:30 +0100

    update

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { TagService } from './tag.service';
 
 @Controller('tags')
-
-
 export class TagController {
+  constructor(private readonly tagService: TagService) {}
 
-   @Get()
-   getAll() {
-    return ['ai', 'typescript', 'javascript'];
-   }
+  @Get()
+  getAll() {
+    return this.tagService.getAll();
+  }
 }
