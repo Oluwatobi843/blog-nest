e01a9977cdf359a317d94116e2e79517a2fd2726
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2026-01-29 22:18:58 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TagModule } from './tag/tag.module';  
+import { TagModule } from './tag/tag.module';        
 
 
 @Module({
