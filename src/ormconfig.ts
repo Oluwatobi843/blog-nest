868773395f9c17922f5c4c59ec063ce2521a9707
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2026-01-30 15:31:32 +0100

    update

---
+++ src/ormconfig.ts
@@ -0,0 +1,14 @@
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions.js";
+
+
+const config: PostgresConnectionOptions = {
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'devuser',
+        password: '1234',
+        database: 'blog'
+}
+
+
+export default config;
